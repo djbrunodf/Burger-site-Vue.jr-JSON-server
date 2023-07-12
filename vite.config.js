125b9d05d932fcb3f5e5321c70629743cf2d493a
Bruno Gustavo <brunogustavolacerda@gmail.com>
2023-07-12 16:53:44 -0300

    change vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Burger-site-Vue.jr-JSON-server/',
   plugins: [vue()],
   resolve: {
     alias: {
